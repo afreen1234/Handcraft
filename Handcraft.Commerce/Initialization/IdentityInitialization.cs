59f97329d7660f52fe9b53bf4f9b55c6e0e5938a
Afreen Khan <afreen@techspian.com>
2022-10-02 22:49:10 +0530

    Adding Ecommerce package

---
+++ Handcraft.Commerce/Initialization/IdentityInitialization.cs
@@ -0,0 +1,34 @@
+﻿using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Linq;
+using System.Web;
+
+namespace Handcraft.Commerce.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class IdentityInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.AddTransient<IOwinContext>(locator => HttpContext.Current.GetOwinContext());
+            context.Services.AddTransient<ApplicationUserManager<ApplicationUser>>(locator => locator.GetInstance<IOwinContext>().GetUserManager<ApplicationUserManager<ApplicationUser>>());
+            context.Services.AddTransient<IAuthenticationManager>(locator => locator.GetInstance<IOwinContext>().Authentication);
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+            //Add initialization logic, this method is called once after CMS has been initialized
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+    }
+}
---
+++ Handcraft.Commerce/Startup.cs
@@ -0,0 +1,58 @@
+﻿using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.ServiceLocation;
+using Mediachase.Data.Provider;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Handcraft.Commerce
+{
+    public class Startup
+    {
+        private readonly IConnectionStringHandler _connectionStringHandler;
+        string loginPath = "/util/login.aspx";
+
+        public Startup() : this(ServiceLocator.Current.GetInstance<IConnectionStringHandler>())
+        {
+            // Parameterless constructor required by OWIN.
+        }
+
+        public Startup(IConnectionStringHandler connectionStringHandler)
+        {
+            _connectionStringHandler = connectionStringHandler;
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.AddCmsAspNetIdentity<ApplicationUser>(new ApplicationOptions
+            {
+                ConnectionStringName = _connectionStringHandler.Commerce.Name
+            });
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider.
+            // Configure the sign in cookie.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager<ApplicationUser>, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => manager.GenerateUserIdentityAsync(user)),
+                    OnApplyRedirect = context => context.Response.Redirect(context.RedirectUri),
+                    //OnResponseSignOut = context => context.Response.Redirect(UrlResolver.Current.GetUrl(ContentReference.StartPage))
+                }
+            });
+        }
+    }
+}
